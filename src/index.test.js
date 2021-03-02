5eafb80999f0aabbecd1ceba7d649cca056fd5d0
Gabriel Sheeley <iammcgaber@gmail.com>
2021-03-02 15:29:44 -0500

    Make updates based on feedback from coworker about how to simplify the code

--- src/index.test.js
+++ src/index.test.js
@@ -3,69 +3,62 @@ import '@testing-library/jest-dom/extend-expect'
 import { JSDOM } from 'jsdom'
 import path from 'path'
 
+const HTML_UNDER_TEST = path.resolve(__dirname, './index.html')
+const options = { 
+    runScripts: 'dangerously', 
+    resources: "usable", 
+    includeNodeLocations: true
+}
+
 let dom
-let container
-let fullDom
-let eventListenerCallback
+let body
 
 describe('index.html', () => {
   beforeEach((done) => {
-    JSDOM.fromFile(path.resolve(__dirname, './index.html'), { runScripts: 'dangerously', resources: "usable", includeNodeLocations: true }).then(builtDom => {
-        container = builtDom.window.document.body
-        dom = builtDom.serialize()
-        fullDom = builtDom
-    }).then(() => done())
-  })
-
-  afterEach(() => {
-    fullDom.window.document.removeEventListener('DOMContentLoaded', eventListenerCallback)
+    JSDOM.fromFile(HTML_UNDER_TEST, options).then(domObject => {
+        body = domObject.window.document.body
+        dom = domObject
+        dom.window.document.addEventListener('DOMContentLoaded', () => done())
+    })
   })
 
   it('renders a heading element', () => {
-    expect(container.querySelector('h1')).not.toBeNull()
-    expect(getByText(container, 'Pun Generator')).toBeInTheDocument()
+    expect(body.querySelector('h1')).not.toBeNull()
+    expect(getByText(body, 'Pun Generator')).toBeInTheDocument()
   })
 
   it('renders a button element', () => {
-    expect(container.querySelector('button')).not.toBeNull()
-    expect(getByText(container, 'Click me for a terrible pun')).toBeInTheDocument()
+    expect(body.querySelector('button')).not.toBeNull()
+    expect(getByText(body, 'Click me for a terrible pun')).toBeInTheDocument()
   })
 
   it('renders a new paragraph via JavaScript when the button is clicked', () => {
-    const button = getByText(container, 'Click me for a terrible pun')
+    const button = getByText(body, 'Click me for a terrible pun')
 
-    eventListenerCallback = () => {
-      fireEvent.click(button)
-      let generatedParagraphs = container.querySelectorAll('#pun-container p')
-      expect(generatedParagraphs.length).toBe(1)
+    fireEvent.click(button)
+    let generatedParagraphs = body.querySelectorAll('#pun-container p')
+    expect(generatedParagraphs.length).toBe(1)
 
-      fireEvent.click(button)
-      generatedParagraphs = container.querySelectorAll('#pun-container p')
-      expect(generatedParagraphs.length).toBe(2)
+    fireEvent.click(button)
+    generatedParagraphs = body.querySelectorAll('#pun-container p')
+    expect(generatedParagraphs.length).toBe(2)
 
-      fireEvent.click(button)
-      generatedParagraphs = container.querySelectorAll('#pun-container p')
-      expect(generatedParagraphs.length).toBe(3)
-  }
-    
-    fullDom.window.document.addEventListener('DOMContentLoaded', eventListenerCallback);
+    fireEvent.click(button)
+    generatedParagraphs = body.querySelectorAll('#pun-container p')
+    expect(generatedParagraphs.length).toBe(3)
   })
 
   it('should create a new H2 element', () => {
-    eventListenerCallback = () => {
-      const hTwo = getByText(container, "Hi There")
+      const hTwo = getByText(body, "Hi There")
+
       expect(hTwo).not.toBeNull();
       expect(hTwo).toBeInTheDocument()
-    }
-    fullDom.window.document.addEventListener('DOMContentLoaded', eventListenerCallback)
   })
 
   it('should be able to test jQuery', () => {
-    eventListenerCallback = () => {
-      const hTwo = getByText(container, "Yo yo yo")
+      const hTwo = getByText(body, "Yo yo yo")
+      
       expect(hTwo).not.toBeNull();
       expect(hTwo).toBeInTheDocument()
-    }
-    fullDom.window.document.addEventListener('DOMContentLoaded', eventListenerCallback)
   })
 })
